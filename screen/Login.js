3403cfbd41d03fe629d445f0b5a988bf5e76fe65
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2024-03-26 15:35:00 +0530

    login

---
+++ screen/Login.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import { Button, Image, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+
+
+function Login({ navigation }) {
+    return (
+        <SafeAreaView>
+            <View style={styles.container}>
+                <View style={styles.box}>
+                    <Text style={styles.text} >Company Logo</Text>
+
+                    <View style={styles.login}>
+                        <Text style={{ fontSize: 40, color: 'black', fontWeight: 600, marginTop: 40 }} >Login</Text>
+                        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-evenly' }}>
+                            <TextInput placeholder='Email' style={styles.textinput} />
+                            <TextInput placeholder='Password' style={styles.textinput} secureTextEntry={true} />
+                        </View>
+                      <View>
+                            <Pressable style={styles.button} >
+                                <Text style={{ color: 'white', fontSize: 15 }}>Login</Text>
+                            </Pressable>
+                            <Text style={{textAlign:'center',paddingTop:10}}>or Login with</Text>
+                            <View style={styles.header} >
+                                <AntDesign style={{ margin: 20 }} name="facebook-square" color={'black'} size={22} />
+                                <AntDesign style={{ margin: 20 }} name="instagram" color={'black'} size={22} />
+                                <AntDesign style={{ margin: 20 }} name="twitter" color={'black'} size={22} />
+                            </View>
+                            <Text>Don't have any account?<Text style={{ textDecorationLine: 'underline' }} onPress={() => navigation.navigate("signup")}>signup</Text></Text>
+                      </View>
+
+                    </View>
+                </View>
+
+            </View>
+        </SafeAreaView>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'black',
+
+        height: '100%',
+        width: '100%'
+    },
+    box: {
+        height: '100%',
+        width: '100%',
+
+        alignItems: 'center',
+        padding: 50,
+        margin: 30
+    },
+
+
+    text: {
+        fontSize: 20,
+        color: 'white',
+        borderWidth: 2,
+        borderColor: 'white',
+        paddingTop: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 10
+
+    },
+    login: {
+        width: '160%',
+        height: '110',
+        backgroundColor: '#e5eae7',
+        marginTop: 80,
+        borderWidth: 4,
+        borderColor: 'black',
+        borderTopLeftRadius: 150,
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+
+
+    },
+    textinput: {
+        width: 250,
+        height: 50,
+        borderWidth: 2,
+        borderRadius: 5,
+        borderColor: 'white',
+        backgroundColor: 'white',
+        // marginTop:50,
+        fontSize: 15,
+        color: 'black'
+
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 15,
+        paddingHorizontal: 95,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'black',
+
+    },
+})
+
+export default Login
---
+++ screen/Signup.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import { Button, Image, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+
+function Signup({ navigation }) {
+    
+    return (
+        <SafeAreaView>
+            <View style={styles.container}>
+                <View style={styles.box}>
+                    <View style={styles.header} >
+                        <MaterialCommunityIcons name="arrow-left" color={'white'} size={32}  onPress={()=>navigation.navigate("login")} />
+                        <Text style={styles.text} > Sign Up</Text>
+                    </View>
+
+                    <View style={styles.login}>
+                        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-evenly', marginTop: 20 }}>
+                            <TextInput placeholder='Name' style={styles.textinput} />
+                            <TextInput placeholder='Email' style={styles.textinput} />
+                            <TextInput placeholder='Password' style={styles.textinput} secureTextEntry={true} />
+                            <TextInput placeholder='ConfirmPassword' style={styles.textinput} secureTextEntry={true} />
+                            {/* <Button title='Signup' color={'black'} style={{ padding: 80, fontSize: 40 }}  ></Button> */}
+                            <Pressable style={styles.button} >
+                                <Text style={{color:'white',fontSize:15}}>Sign Up</Text>
+                            </Pressable>
+                            <Text>You have an account?<Text style={{textDecorationLine:'underline'}} onPress={() => navigation.navigate("login")}>Login</Text></Text>
+                        </View>
+                    </View>
+                </View>
+
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'black',
+
+        height: '100%',
+        width: '100%'
+    },
+    box: {
+        height: '100%',
+        width: '100%',
+        top: 0,
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        padding: 50,
+        margin: 30
+    },
+
+
+    text: {
+        fontSize: 40,
+        color: 'white',
+
+        paddingTop: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 10
+
+    },
+    login: {
+        width: '160%',
+        height: '110%',
+        backgroundColor: '#e5eae7',
+        marginTop: 80,
+        borderWidth: 4,
+        borderColor: 'black',
+        borderTopLeftRadius: 150,
+        alignItems: 'center',
+
+
+    },
+    textinput: {
+        width: 250,
+        height: 50,
+        borderWidth: 2,
+        borderRadius: 5,
+        borderColor: 'white',
+        backgroundColor: 'white',
+        // marginTop:50,
+        fontSize: 15,
+        color: 'black'
+
+    },
+    header:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 15,
+        paddingHorizontal: 95,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'black',
+        
+      },
+    
+
+})
+export default Signup
